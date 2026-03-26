a092da9b7dd50fd3ca812e95b7eda9033263e009
NocturneSterling <oscar.f.bruce@gmail.com>
2026-03-26 21:03:58 +0000

    modified:   RunC.sh

--- RunC.sh
+++ RunC.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-#script to turn compiling and running C++ into one command
-fileName=$1 
+#Compiles and then runs C++, Java or bash files. Resets the color in case of overflow
+RESETTEXT="\e[0m" #reset text color
+RESETBACK='\033[0m' #reset background color
 
-g++ $fileName
-./$fileName
+file=$1
+
+if [[ $file =~ \.cpp$ ]]; do
+	g++ ${file}
+	./a.out
+elif [[ $file =~ \.java$ ]]; then
+	javac ${file}
+    java ${file}
+elif [[ $file =~ \.sh$ ]]; then
+	./${file}
+elif [[ $file =~ \.py$ ]]; then
+	python ${file}
+else
+	printf "file type not .cpp, .js or .sh
+fi
+
+printf "${RESETEXT}${RESETBACK}"
